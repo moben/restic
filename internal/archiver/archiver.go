9ea08e188a44409289623daf4f57143133797d02
Benedikt Morbach <benedikt.morbach@googlemail.com>
2021-01-24 23:39:12 +0100

    hack: Increase file read concurrency

--- internal/archiver/archiver.go
+++ internal/archiver/archiver.go
@@ -106,11 +106,11 @@ func (o Options) ApplyDefaults() Options {
 		// two is a sweet spot for almost all situations. We've done some
 		// experiments documented here:
 		// https://github.com/borgbackup/borg/issues/3500
-		o.FileReadConcurrency = 2
+		o.FileReadConcurrency = 16
 	}
 
 	if o.SaveBlobConcurrency == 0 {
-		o.SaveBlobConcurrency = uint(runtime.NumCPU())
+		o.SaveBlobConcurrency = uint(2 * runtime.NumCPU())
 	}
 
 	if o.SaveTreeConcurrency == 0 {
